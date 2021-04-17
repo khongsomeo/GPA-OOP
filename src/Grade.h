b1b7c324b5c37a8d7ef23361ff0e33d17fa7a00a
trhgquan <quanthcstpbt@gmail.com>
2021-04-17 21:10:55 +0700

    added Grade.h - new way to calculate GPA.

---
+++ src/Grade.h
@@ -0,0 +1,131 @@
+/**
+ * Grade implementation.
+ *
+ * Code by @trhgquan - https://github.com/trhgquan
+ */
+
+#ifndef GRADE_H
+#define GRADE_H
+
+class Grade {
+private:
+  double _grade;
+public:
+  Grade() {
+    _grade = 0;
+  }
+
+  Grade(double grade) {
+    _grade = grade;
+  }
+
+  Grade(const Grade& other) {
+    _grade = other._grade;
+  }
+  
+  Grade& operator=(const Grade& other) {
+    _grade = other._grade;
+    return *this;
+  }
+
+  Grade& operator=(double grade) {
+    _grade = grade;
+    return *this;
+  }
+
+  ~Grade() {
+    // Do nothing.
+  }
+
+public:
+  // Convert grade to scales.
+  
+  double to4Scale() {
+    if (_grade < 3.0) return 0.0;
+    if (_grade < 4.0) return 1.0;
+    if (_grade < 5.0) return 1.5;
+    if (_grade < 6.0) return 2.0;
+    if (_grade < 7.0) return 2.5;
+    if (_grade < 8.0) return 3.0;
+    if (_grade < 9.0) return 3.5;
+    return 4.0;
+  }
+
+  std::string toAScale() {
+    if (_grade < 3.0) return "F";
+    if (_grade < 4.0) return "D";
+    if (_grade < 5.0) return "D+";
+    if (_grade < 6.0) return "C";
+    if (_grade < 7.0) return "B";
+    if (_grade < 8.0) return "B+";
+    if (_grade < 9.0) return "A";
+    return "A+";
+  }
+
+  // Comparision.
+
+  bool operator<(const Grade& other) const {
+    return _grade < other._grade;
+  }
+
+  bool operator>(const Grade& other) const {
+    return _grade > other._grade;
+  }
+
+  bool operator>=(const Grade& other) const {
+    return _grade >= other._grade;
+  }
+
+  bool operator<=(const Grade& other) const {
+    return _grade <= other._grade;
+  }
+
+  bool operator==(const Grade& other) const {
+    return _grade == other._grade;
+  }
+
+  // Arithmetic
+  Grade operator+(const Grade& other) const {
+    return Grade(_grade + other._grade);
+  }
+  
+  Grade& operator+=(const Grade& other) {
+    _grade += other._grade;
+    return *this;
+  }
+  
+  Grade operator-(const Grade& other) const {
+    return Grade(_grade - other._grade);
+  }
+  
+  Grade& operator-=(const Grade& other) {
+    _grade -= other._grade;
+    return *this;
+  }
+  
+  Grade operator*(const Grade& other) const {
+    return Grade(_grade * other._grade);
+  }
+  
+  Grade& operator*=(const Grade& other) {
+    _grade *= other._grade;
+    return *this;
+  }
+  
+  Grade operator/(const Grade& other) const {
+    return Grade(_grade / other._grade);
+  }
+  
+  Grade& operator/=(const Grade& other) {
+    _grade /= other._grade;
+    return *this;
+  }
+
+  // Print Grade to the ostream.
+  friend std::ostream& operator<<(std::ostream& out, const Grade& grade) {
+    out << _grade;
+    return out;
+  }
+};
+
+#endif
