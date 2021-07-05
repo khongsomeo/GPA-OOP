9916a290f9ccc16e96180a52c08220a13fbe91a2
Hwang S. Wan <mrthedzam@gmail.com>
2021-07-05 22:58:47 +0700

    Update data and testcases

--- tests/test.sh
+++ tests/test.sh
@@ -16,11 +16,11 @@ declare -a test_seeds
 
 test_seeds[0]="./main data/19120338.csv"
 test_seeds[1]="./main data/19120338.csv --gpa"
-test_seeds[2]="./main data/19120338.csv --specific CSC"
+test_seeds[2]="./main data/19120338.csv --specific data/specific.txt"
 test_seeds[3]="./main data/19120338.csv --ignore data/ignore.txt"
 test_seeds[4]="./main data/19120338.csv --csv"
 test_seeds[5]="./main data/19120338.csv --gpa --csv"
-test_seeds[6]="./main data/19120338.csv --specific CSC --csv"
+test_seeds[6]="./main data/19120338.csv --specific data/specific.txt --csv"
 test_seeds[7]="./main data/19120338.csv --ignore data/ignore.txt --csv"
 test_seeds[8]="./main --gpa"
 test_seeds[9]="./main data/19120338.csv --specific --csv"
